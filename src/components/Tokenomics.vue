a7bb6c52b5f1ec1565c170ea0a1d547ebb2d948c
Dea Kil <deakil@moonrat.finance>
2021-03-30 23:09:56 +0700

    add alert

--- src/components/Tokenomics.vue
+++ src/components/Tokenomics.vue
@@ -19,17 +19,26 @@
                         <div class="col-lg-7 col-md-6 tokenomic-content">
                             <div class="about-txt">
                                 <h3 style="color:#04cd33;">Token Stuff</h3>
-                                <p>Total Supply: 1 Quadrillion MOON RAT<br>
-                                <ul>
+                                <p>Total Supply: 1 Quadrillion MOON RAT<br></p>
+
+                                <ul class="ul-token-stuff">
                                     <li>80% for the fair launch. We add 80% of token to LP on Pancakeswap</li>
-                                    <li>5% for airdrop </li>
-                                    <li>15% for the foundation: Team only gets 4%, and 1% for marketing. We have a plan to burn the rest 10% of the foundation.</li>
-                                    <li><a href="https://docs.moonrat.finance/tokenomics/introduction" target="_blank">View more </a></li>
+
+                                    <li>5% for airdrop</li>
+
+                                    <li>15% for the foundation: Team only gets 4%, and 1% for marketing. We have a plan
+                                        to burn the rest 10% of the foundation.
+                                    </li>
+
+
+                                    <a href="https://docs.moonrat.finance/tokenomics/introduction" target="_blank">
+                                        View more </a>
+
                                 </ul>
-                                
-                                    <!-- Circulating Supply: 973,840,412,032,907 $MRAT<br>
-                                    Holders:  $MRAT<br> -->
-                                </p>
+
+                                <!-- Circulating Supply: 973,840,412,032,907 $MRAT<br>
+                                Holders:  $MRAT<br> -->
+
                                 <!-- <h3 style="color:#04cd33;" class="mt-5">Burn Schedule</h3>
                                 <p>Total MOON RAT Burned:  $MRAT</p> -->
                             </div>
