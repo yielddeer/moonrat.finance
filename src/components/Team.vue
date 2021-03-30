f3a3824e4e4f4ba7423de41dc757eaabd2eadb15
Dea Kil <deakil@moonrat.finance>
2021-03-30 23:22:51 +0700

    add alert

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -6,21 +6,29 @@
                     <h3>TE<span>A</span>M</h3>
                 </div>
             </div>
+
             <div class="row team-pa">
+                <div class="m-auto team-item text-center">
+                    <img :src="require('@/assets/images/thuglife.png')" alt="team-member" class="img-fluid image-team">
+                    <h3>Community</h3>
+                </div>
+            </div>
+
+            <div class="row team-pa" style="padding-top: 10px!important;">
                 <div class="col-lg-3 col-md-3 col-sm-4 m-auto team-item text-center">
                     <img :src="require('@/assets/images/thuglife.png')" alt="team-member" class="img-fluid image-team">
                     <h3>Alan</h3>
-                    <p>Core Team</p>
+                    <p>DApp Specialist</p>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-4 m-auto team-item text-center">
                     <img :src="require('@/assets/images/thuglife.png')" alt="team-member" class="img-fluid image-team">
                     <h3>Stoic Citium</h3>
-                    <p>Core Team</p>
+                    <p>Head Of Marketing</p>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-4 m-auto team-item text-center">
                     <img :src="require('@/assets/images/thuglife.png')" alt="team-member" class="img-fluid image-team">
                     <h3>DeaKil</h3>
-                    <p>Website Developer</p>
+                    <p>Product Developer</p>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-4 m-auto team-item text-center">
                     <img :src="require('@/assets/images/thuglife.png')" alt="team-member" class="img-fluid image-team">
